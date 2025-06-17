dff190cccfedf60663416f2157c7a2be8836666e
liudan <403644786@qq.com>
2025-06-17 14:26:42 +0800

    修改了chat_tamplate

--- eagle/traineagle3/cnets.py
+++ eagle/traineagle3/cnets.py
@@ -574,8 +574,8 @@ class Model(nn.Module):
                 }
                 for i in range(len(examples['id'])):
                     messages = [
-                        {"role": "system",
-                         "content": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."},
+                        # {"role": "system",
+                        #  "content": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."},
                     ]
                     convroles = ["user", "assistant"]
                     roles = {"human": "user", "gpt": "assistant"}
--- eagle/traineagle3/main.py
+++ eagle/traineagle3/main.py
@@ -102,8 +102,8 @@ def build_dataset_rank(tokenizer, datapath):
         for i in range(len(examples['id'])):
             # 构建系统提示词
             messages = [
-                {"role": "system",
-                 "content": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."},
+                # {"role": "system",
+                #  "content": "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."},
             ]
             
             # 对话角色映射
